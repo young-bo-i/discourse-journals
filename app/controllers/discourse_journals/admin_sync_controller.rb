71c5272e2875bb34ecc25ce4da6901f470f2791d
pengjiebo <pengjiebo@beisen.com>
2026-01-19 20:15:24 +0800

    1

--- app/controllers/discourse_journals/admin_sync_controller.rb
+++ app/controllers/discourse_journals/admin_sync_controller.rb
@@ -9,7 +9,9 @@ module DiscourseJournals
     def create
       mode = params[:mode] # "first_page" 或 "all_pages"
       api_url = params[:api_url].presence || SiteSetting.discourse_journals_api_url
-      filters = (params[:filters] || {}).to_h.with_indifferent_access
+      
+      # 提取并清理 filters 参数
+      filters = extract_filters
 
       if api_url.blank?
         return render_json_error("请在设置中配置 API URL")
@@ -83,18 +85,37 @@ module DiscourseJournals
 
     private
 
+    def extract_filters
+      filters = {}
+      
+      if params[:filters].present?
+        filter_params = params[:filters]
+        
+        # 提取每个筛选条件
+        filters[:q] = filter_params[:q] if filter_params[:q].present?
+        filters[:in_doaj] = filter_params[:in_doaj] if filter_params[:in_doaj].present?
+        filters[:in_nlm] = filter_params[:in_nlm] if filter_params[:in_nlm].present?
+        filters[:has_wikidata] = filter_params[:has_wikidata] if filter_params[:has_wikidata].present?
+        filters[:is_open_access] = filter_params[:is_open_access] if filter_params[:is_open_access].present?
+        filters[:sort_by] = filter_params[:sort_by] if filter_params[:sort_by].present?
+        filters[:sort_order] = filter_params[:sort_order] if filter_params[:sort_order].present?
+      end
+      
+      filters
+    end
+
     def build_filter_description(filters)
       return "" if filters.blank?
 
       parts = []
-      parts << "关键词: #{filters['q']}" if filters['q'].present?
-      parts << "DOAJ期刊" if filters['in_doaj'] == true
-      parts << "非DOAJ期刊" if filters['in_doaj'] == false
-      parts << "NLM期刊" if filters['in_nlm'] == true
-      parts << "非NLM期刊" if filters['in_nlm'] == false
-      parts << "有Wikidata" if filters['has_wikidata'] == true
-      parts << "开放获取" if filters['is_open_access'] == true
-      parts << "非开放获取" if filters['is_open_access'] == false
+      parts << "关键词: #{filters[:q]}" if filters[:q].present?
+      parts << "DOAJ期刊" if filters[:in_doaj] == true || filters[:in_doaj] == "true"
+      parts << "非DOAJ期刊" if filters[:in_doaj] == false || filters[:in_doaj] == "false"
+      parts << "NLM期刊" if filters[:in_nlm] == true || filters[:in_nlm] == "true"
+      parts << "非NLM期刊" if filters[:in_nlm] == false || filters[:in_nlm] == "false"
+      parts << "有Wikidata" if filters[:has_wikidata] == true || filters[:has_wikidata] == "true"
+      parts << "开放获取" if filters[:is_open_access] == true || filters[:is_open_access] == "true"
+      parts << "非开放获取" if filters[:is_open_access] == false || filters[:is_open_access] == "false"
 
       parts.any? ? "（筛选：#{parts.join('、')}）" : ""
     end
